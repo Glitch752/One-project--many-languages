73171ffe1ba021246c55b425221a75b8d270d893
Glitch752 <xxGlitch752xx@gmail.com>
2022-03-29 20:55:52 -0500

    Finished game

--- 2.2) Python GUI/main.py
+++ 2.2) Python GUI/main.py
@@ -12,7 +12,7 @@ def start():
 
     app = App(title="Tic Tac Toe", width=windowWidth, height=windowHeight)
 
-    Text(app, text="Tic Tac Toe", size=titleHeight - 10, color="black")
+    text = Text(app, text="", size=titleHeight - 30, color="black")
 
     buttonBoxes = []
     buttons = []
@@ -31,20 +31,67 @@ def start():
         button.text_size = 24
         buttons.append(button)
 
-    main(app, buttons)
+    main(app, buttons, text)
     app.display()
 
-def main(app, buttons):
+def main(app, buttons, text):
     gameboard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    update_buttons(app, gameboard, buttons)
+    gameState = "XTURN"
 
-def update_buttons(app, gameboard, buttons):
+    update_text(gameState, text)
+
+    update_buttons(app, gameboard, buttons, text, gameState)
+
+def update_buttons(app, gameboard, buttons, text, gameState):
     for i in range(9):
         buttons[i].text = gameboard[i]
-        buttons[i]._on_press = lambda i=i: button_clicked(app, i, gameboard, buttons)
+        buttons[i].update_command(button_clicked, args=[app, i, gameboard, buttons, text, gameState])
 
-def button_clicked(app, i, gameboard, buttons):
-    gameboard[i] = "X"
-    update_buttons(app, gameboard, buttons)
+def update_text(gameState, text):
+    if gameState == "XTURN":
+        text.value = "X's turn"
+    elif gameState == "OTURN":
+        text.value = "O's turn"
+    elif gameState == "XWON":
+        text.value = "X wins! Click to restart."
+    elif gameState == "OWON":
+        text.value = "O wins! Click to restart."
+    else:
+        text.value = "Draw! Click to restart."
+
+def button_clicked(app, i, gameboard, buttons, text, gameState):
+    if not gameState == "XTURN" and not gameState == "OTURN":
+        main(app, buttons, text)
+        return
+    
+    if not gameboard[i] == " ": return
+    
+    gameboard[i] = "X" if gameState == "XTURN" else "O"
+    gameState = "XTURN" if gameState == "OTURN" else "OTURN"
+
+    win = check_win(gameboard)
+    if win == "X":
+        gameState = "XWON"
+    elif win == "O":
+        gameState = "OWON"
+    elif win == "DRAW":
+        gameState = "DRAW"
+
+    update_text(gameState, text)
+
+    update_buttons(app, gameboard, buttons, text, gameState)
+
+def check_win(gameboard):
+    winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+    for combo in winCombos:
+        if gameboard[combo[0]] == gameboard[combo[1]] == gameboard[combo[2]] and gameboard[combo[0]] != " ":
+            return gameboard[combo[0]]
+    
+    for i in range(9):
+        if gameboard[i] == " ":
+            return None
+
+    return "DRAW"
 
 start()
